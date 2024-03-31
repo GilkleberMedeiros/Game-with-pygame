aaaf90eb711342ca24cd6de6277c4bff7824ae1e
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-03-31 15:05:04 -0300

    Colisão adicionada, espero que continue funcionando :)

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player():
         self.dt = 0
         self.pos = [x, y, width, heigth]
         self.rect = self.screen.blit(Files.PlayerIdle, (self.pos[0], self.pos[1]))
-        Files.rects.append(tuple(self.pos))
+        Files.rects.append(self.rect)
 
     def movement(self):
         clock = time.Clock()
@@ -28,20 +28,37 @@ class Player():
         self.dt = clock.tick(60) / 1000
 
     def redraw(self):
-        #draw.rect(self.screen, (100, 100, 100), tuple(self.rect))
         self.rect = self.screen.blit(Files.PlayerIdle, (self.pos[0], self.pos[1]))
     
-    def colission(self, f_pos, pos: str):
-        if pos.lower() == "x":
+    def colission(self, f_pos, direc: str):
+        if direc.lower() == "x":
             self.rect[0] = f_pos
-        elif pos.lower() == "y":
+        elif direc.lower() == "y":
             self.rect[1] = f_pos
         
-        print(Files.rects)
-        if self.rect.collidelist(Files.rects) == -1:
-            #print(Files.rects)
-            return True
-        return False
+        colission_list = self.get_colission_list(self.rect, Files.rects)
+        return self.rect.collidelist(colission_list) == -1
+    
+    def get_colission_list(self, rect: Rect, colission_list: list) -> list:
+        """
+            Verify if a rect is in a colission list
+            with 1 and -1 tolerance
+            param: rect, list
+            return: list
+        """
+        return_list = colission_list.copy()
+        for i in return_list:
+            count = 0
+            for j in i:
+                if j < rect[count] - 1 and j > rect[count] + 1:
+                    break
+                count += 1
+            else:
+                return_list.remove(i)
+
+        return return_list
+
+
         
 
 class Wall():
@@ -49,6 +66,7 @@ class Wall():
         self.screen = screen
         self.rect = [x, y, width, heigth]
         draw.rect(self.screen, (100, 100, 100), tuple(self.rect))
+        Files.rects.append(Rect(self.rect))
 
     def redraw(self):
         draw.rect(self.screen, (100, 100, 100), tuple(self.rect))
