a29eb2fc9330f0291db1baa903c12283c1664d61
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-04-28 19:19:50 -0300

    Colisão consertada e funcionando bem dessa vez

--- player.py
+++ player.py
@@ -9,35 +9,68 @@ class Player():
         self.dt = 0
         self.pos = [x, y, width, heigth]
         self.rect = self.screen.blit(Files.PlayerIdle, (self.pos[0], self.pos[1]))
-        Files.rects.append(self.rect)
 
-    def movement(self):
+    def movement(self) -> None:
         clock = time.Clock()
 
         # Move the Player
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.colission(self.pos[0] - 1, "x"):
+        if keys[K_LEFT] and self.colission((self.pos[0] / 1) - 1, "x"):
             self.pos[0] -= 30 * self.dt
-        if keys[K_RIGHT] and self.colission(self.pos[0] + 1, "x"):
+        if keys[K_RIGHT] and self.colission((self.pos[0] / 1) + 1, "x"):
             self.pos[0] += 30 * self.dt
-        if keys[K_DOWN] and self.colission(self.pos[1] + 1, "y"):
+        if keys[K_DOWN] and self.colission((self.pos[1] / 1) + 1, "y"):
             self.pos[1] += 30 * self.dt
-        if keys[K_UP] and self.colission(self.pos[1] + 1, "y"):
+        if keys[K_UP] and self.colission((self.pos[1] / 1) - 1, "y"):
             self.pos[1] -= 30 * self.dt
         
         self.dt = clock.tick(60) / 1000
 
-    def redraw(self):
+    def redraw(self) -> None:
         self.rect = self.screen.blit(Files.PlayerIdle, (self.pos[0], self.pos[1]))
     
-    def colission(self, f_pos, direc: str):
+    def colission(self, f_pos: float | int, direc: str) -> bool:
+        """
+        Check if player is colliding with another game object from
+        Files.rects
+        param: float | int, str
+        return: bool
+        """
         if direc.lower() == "x":
             self.rect[0] = f_pos
         elif direc.lower() == "y":
             self.rect[1] = f_pos
-        
-        colission_list = self.get_colission_list(self.rect, Files.rects)
-        return self.rect.collidelist(colission_list) == -1
+
+        # Corners of player rect
+        corners = [[self.rect[0], self.rect[1]], # :left upper
+                  [self.rect[0] + self.rect[2], self.rect[1]], # :right upper 
+                  [self.rect[0], self.rect[1] + self.rect[3]], # :left lower
+                  [self.rect[0] + self.rect[2], self.rect[1] + self.rect[3]]] # :right lower
+        for i in Files.rects:
+            # xy1 True if left upper corner inside of another rect
+            xy1 = (corners[0][0] <= (i[0] + i[2]) and \
+                   corners[0][0] >= i[0]) and \
+                  (corners[0][1] <= (i[1] + i[3]) and \
+                   corners[0][1] >= i[1])
+            # xy2 True if right upper corner inside of another rect
+            xy2 = (corners[1][0] <= (i[0] + i[2]) and \
+                   corners[1][0] >= i[0]) and \
+                  (corners[1][1] <= (i[1] + i[3]) and \
+                   corners[1][1] >= i[1])
+            # xy3 True if left lower corner inside of another rect
+            xy3 = (corners[2][0] <= (i[0] + i[2]) and \
+                   corners[2][0] >= i[0]) and \
+                  (corners[2][1] <= (i[1] + i[3]) and \
+                   corners[2][1] >= i[1])
+            # xy4 True if right lower corner inside of another rect
+            xy4 = (corners[3][0] <= (i[0] + i[2]) and \
+                   corners[3][0] >= i[0]) and \
+                  (corners[3][1] <= (i[1] + i[3]) and \
+                   corners[3][1] >= i[1])
+            if xy1 or xy2 or xy3 or xy4:
+                return False
+        return True
+
     
     def get_colission_list(self, rect: Rect, colission_list: list) -> list:
         """
@@ -71,5 +104,20 @@ class Wall():
     def redraw(self):
         draw.rect(self.screen, (100, 100, 100), tuple(self.rect))
 
-    
-    
+
+def colission_other():
+    """Check if every point of a rect is inside of another"""
+    points = []
+    for i in range(self.rect[0], self.rect[0] + self.rect[2] + 1):
+        for j in range(self.rect[1], self.rect[1] + self.rect[3] + 1):
+            points.append([i, j])
+    colission_list = Files.rects
+    for i in colission_list:
+        for j in points:
+            x = (j[0] <= (i[0] + i[2]) and \
+                j[0] >= i[0])
+            y = (j[1] <= (i[1] + i[3]) and \
+                j[1] >= i[1])
+            if x and y:
+                return False
+    return True
