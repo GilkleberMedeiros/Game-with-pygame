b286ee0c1f68c89e9ff585192880b6cf3069aef7
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-06-29 17:12:49 -0300

    Correções nos caminhos

--- Assets/Maps/map/Brick_Black.tsx
+++ Assets/Maps/map/Brick_Black.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
+
 <tileset version="1.10" tiledversion="1.10.2" name="Brick_Black" tilewidth="32" tileheight="32" tilecount="1" columns="1">
-    <image source="OneDrive/Documentos/Projetos/Game-with-pygame/Assets/Brick_Black.png" width="32" height="32"/>
+    <image source="../../Sprites/Brick_Black.png" width="32" height="32"/>
 </tileset>
